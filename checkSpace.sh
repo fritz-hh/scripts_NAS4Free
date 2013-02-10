7e2d8408190a86f44d258ea5e7d4b822a4df95f4
fritz-hh <julien.pfefferkorn@googlemail.com>
2013-02-10 18:55:43 +0100

    script signature changed and better robustness
    the mandatorry parameters (fs and threshold) are now optional
    If the fs is not defined, all ZFS fs are monitored
    If the threshold is not defined 80% is assumed
    The script now checks the validity of the arguments

--- checkSpace.sh
+++ checkSpace.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 #############################################################################
-# Script aimed at checking if the quota of the filesystem is nearly reached
+# Script aimed at checking if the quota of the ZFS filesystem is nearly reached
 #
 # Author: fritz from NAS4Free forum
 #
-# Param 1: Filesystem to be monitored
-# Param 2: Threshold in percent above which a notification mail shall be sent
+# Usage: [-f filesystem] [-t threshold]
+#
+#	-f filesystem : the ZFS filesystem (incl. sub-fs) to be checked
+#			default: all ZFS filesystems
+# 	-t threshold : the threshold in percent above which a notification mail shall be sent
+#			value must be between 0 and 99
+#			default: 80 (percent)
 #############################################################################
 
 # Initialization of the script name and path constants
@@ -18,15 +23,65 @@ readonly SCRIPT_PATH=`dirname $0`		# The path to the current script
 . "$SCRIPT_PATH/common/commonMailFcts.sh"
 . "$SCRIPT_PATH/common/commonLockFcts.sh"
 
+# Record the timestamp corresponding to the start of the script execution
+readonly START_TIMESTAMP=`$BIN_DATE +"%s"`
+
 # Set variables corresponding to the input parameters
-readonly FILESYSTEM="$1" 	# name of the filesystem to be monitored
-readonly WARN_THRESHOLD="$2"
+ARGUMENTS="$@"
 
 # Initialization of the constants 
-readonly START_TIMESTAMP=`$BIN_DATE +"%s"`
-readonly FS_WITHOUT_SLASH=`echo "$FILESYSTEM" | sed 's!/!_!'`    	# The fs without '/' that is not allowed in a file name
-readonly LOGFILE="$CFG_LOG_FOLDER/$SCRIPT_NAME.$FS_WITHOUT_SLASH.log"
+FILESYSTEM="" 		# name of the filesystem to be monitored (default: all)
+WARN_THRESHOLD="80"	# space warning threshold default value
+LOGFILE=""		# log file name (will be set in fct "parseOptionalInputParams")
 
+
+################################## 
+# Check script input parameters
+#
+# Params: all parameters of the shell script
+##################################
+parseOptionalInputParams() {
+
+	local fs_without_slash specific_fs
+	specific_fs="0"
+
+	# parse the parameters
+	while getopts ":f:t:" opt; do
+        	case $opt in
+                        f)	fs_without_slash=`echo "$OPTARG" | sed 's!/!_!'`		# value of the fs without '/' that is not allowed in a file name
+ 				LOGFILE="$CFG_LOG_FOLDER/$SCRIPT_NAME.$fs_without_slash.log" 	# value of the log file name
+				specific_fs="1"
+ 				$BIN_ZFS list "$OPTARG" >/dev/null      			# Check if the zfs file system exists
+                                if [ "$?" -eq "0" ] ; then
+                                        FILESYSTEM="$OPTARG"
+                                else
+                                        log_error "$LOGFILE" "Invalid parameter \"$OPTARG\" for option: -f. The ZFS filesystem does not exist."
+                                        return 1
+                                fi ;;
+                	t) 	# first check if a specific fs was set before, otherwise
+				# set the log file name to its default value
+				if [ "$specific_fs" -eq "0" ]; then
+					LOGFILE="$CFG_LOG_FOLDER/$SCRIPT_NAME.log"
+				fi
+				# check if the threshold value is valid
+				echo "$OPTARG" | grep -E '^[0-9]{1,2}$' >/dev/null
+				if [ "$?" -eq "0" ] ; then 
+					WARN_THRESHOLD="$OPTARG" 
+				else
+					log_error "$LOGFILE" "Invalid parameter \"$OPTARG\" for option: -t. Should be an integer between 0 and 99."
+                        		return 1
+				fi ;;
+                	\?)
+				log_error "$LOGFILE" "Invalid option: -$OPTARG"
+                        	return 1 ;;
+                	:)
+				log_error "$LOGFILE" "Option -$OPTARG requires an argument"
+                        	return 1 ;;
+        	esac
+	done
+
+	return 0
+}
 
 ##################################
 # Get the quota for a given filesystem 
@@ -76,6 +131,14 @@ main() {
 	returnCode=0
 	mib=$((1024*1024))
 
+	# Parse the input parameters
+	if ! parseOptionalInputParams $ARGUMENTS; then
+		return 1
+	fi
+
+	log_info "$LOGFILE" "-------------------------------------"
+	log_info "$LOGFILE" "Configured warning threshold: $WARN_THRESHOLD percent"
+	
 	# Check the space for the filesystem (and for all sub-fs recursively)
 	for subfilesystem in `$BIN_ZFS list -H -r -o name $FILESYSTEM`; do
 
@@ -97,12 +160,9 @@ main() {
 	return $returnCode
 }
         
-
-log_info "$LOGFILE" "-------------------------------------"
-log_info "$LOGFILE" "Configured warning threshold: $WARN_THRESHOLD percent"
-
 # run script if possible (lock not existing)
-run_main "$LOGFILE" "$SCRIPT_NAME.$FS_WITHOUT_SLASH"
+fs_without_slash=`echo "$FILESYSTEM" | sed 's!/!_!'`    	# value of the fs without '/' that is not allowed in a file name
+run_main "$LOGFILE" "$SCRIPT_NAME.$fs_without_slash"
 # in case of error, send mail with extract of log in case of error
 [ "$?" -eq "2" ] && `get_log_entries_ts "$LOGFILE" "$START_TIMESTAMP" | sendMail "Checkspace issue"`
 
