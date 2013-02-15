ce32fcece21bf1f18b7db156a4fb623513d6d80a
fritz-hh <julien.pfefferkorn@googlemail.com>
2013-02-15 17:04:29 +0100

    reportFromLog: echo summary
    Echo a summary of the number of errord, warnings that occured

--- reportFromLogs.sh
+++ reportFromLogs.sh
@@ -11,18 +11,16 @@ readonly SCRIPT_PATH=`dirname $0`                # The path to the current scrip
 
 # Import required scripts
 . "$SCRIPT_PATH/config.sh"
-. "$SCRIPT_PATH/commonLogFcts.sh"
+. "$SCRIPT_PATH/common/commonLogFcts.sh"
 
 # Initialization of the constants
-readonly DURATION=604800	# The entries from the last week (duration in sec)
-readonly LOG_FILES="$CFG_LOG_FOLDER/*.log"
+readonly DURATION=604800			# The entries from the last week (duration in sec)
+readonly LOG_FILES="$CFG_LOG_FOLDER/*.log"	# The log files to be considered
 
+# Appending the extract of the logs
 time_limit=`$BIN_DATE -j -v-"$DURATION"S`
 echo "Showing log entries appended after: $time_limit" 
-
-# Generating the report
-for f in $LOG_FILES
-do
+for f in $LOG_FILES; do
 	# Only consider files, not folders
 	if [ -f "$f" ]; then
 		echo ""
@@ -32,4 +30,23 @@ do
 	fi
 done
 
+echo ""
+echo ""
 
+# Computing a summary of the errors / warnings that are recorded in all log files
+echo "Summary:"
+echo "----------------------------"
+for f in $LOG_FILES; do
+	if [ -f "$f" ]; then
+		# check if the log does not contain any new entry
+		get_log_entries "$f" "$DURATION" >/dev/null
+		if [ $? -eq "2" ]; then
+			echo "- $f :	No new log entry available"
+		else
+			# if the log contains any new entry
+			num_warn=`get_log_entries "$f" "$DURATION" | grep -c "WARN"`
+			num_err=`get_log_entries "$f" "$DURATION" | grep -c "ERROR"`	
+			echo "- $f :	WARN:	$num_warn	ERROR: $num_err"
+		fi
+	fi
+done
